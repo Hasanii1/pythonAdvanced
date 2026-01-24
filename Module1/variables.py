000c83a6d942051ba4709ab4a260dc1fff4680ac
Senad Hasani <ssenadhasani@gmail.com>
2026-01-24 12:40:53 +0100

    day1

---
+++ Module1/variables.py
@@ -0,0 +1,75 @@
+
+'''
+temperature = 96
+
+emri = "Erina"
+
+mosha = 30
+
+print(type(temperature))
+
+print(type(emri))
+
+print(type(mosha))
+'''
+
+#Kalkulime
+
+x = 8
+y = 10
+result = x+y
+print(result)
+
+#update values
+
+age = 18
+
+#age = age + 1
+age+=1
+print(age)
+
+#combine values
+
+first_name = "Senad"
+last_name= "Hasani"
+
+full_name = first_name + " " +last_name
+print(full_name)
+
+#array (lists)
+
+
+fav_colors = ["red", "green", "blue", "white","purple"]
+
+first = fav_colors[0]
+
+second = fav_colors[1]
+
+print(first)
+print(second)
+
+#method for lists
+
+#append - add an item  at the end of the list
+
+fav_colors.append("orange")
+print(fav_colors)
+#insert - add element in a specifik index
+
+fav_colors.insert(2, "white")
+print(fav_colors)
+
+#remove
+fav_colors.remove("blue")
+
+del fav_colors[4]
+
+print(fav_colors)
+
+#update
+
+fav_colors[0] = "pink"
+print(fav_colors)
+
+
+
